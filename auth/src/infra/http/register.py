4d688afdac2bdd1f1a838b32689f7a81f8d6e43b
JorgitoR <jorgitouribe133@gmail.com>
2025-02-21 14:56:31 -0500

    feat: add typing

--- auth/src/infra/http/register.py
+++ auth/src/infra/http/register.py
@@ -1,6 +1,8 @@
+from auth.src.utils.schema import BU
 from ecase.service import AuthManagerDepencency
 from fastapi import FastAPI, APIRouter, Depends, HTTPException
 from pydantic import BaseModel, EmailStr
+from typing import Type
 
 class UserLogin(BaseModel):
     email: EmailStr
@@ -11,7 +13,8 @@ class UserRegister(BaseModel):
     password: str
     
 def get_register_router(
-        get_auth_manager: AuthManagerDepencency
+        get_auth_manager: AuthManagerDepencency,
+        schema_register: Type[BU]
 ) -> APIRouter:
     
     router = APIRouter()
@@ -25,7 +28,9 @@ def get_register_router(
             raise HTTPException(status_code=400, detail=str(e))
         
     @router.post("/register", status_code=201, name="register")
-    async def register(user: UserRegister, auth_service: AuthManagerDepencency = Depends(get_auth_manager)):
+    async def register(
+        user: schema_register,  # type: ignore
+        auth_service: AuthManagerDepencency = Depends(get_auth_manager)):
         try:
             response = auth_service.sign_up(user.email, user.password)
             return response
--- auth/src/server.py
+++ auth/src/server.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from utils import schema
 from infra.http.register import get_register_router
 from ecase.service import AuthManagerDepencency
 from infra.db.sql import SQL
@@ -10,6 +11,9 @@ sql_db = SQL(db_conn)
 app = FastAPI(title="Authentication API", version="1.0")
 
 def get_auth_manager():
-    return AuthManagerDepencency(sql_db)
+    yield AuthManagerDepencency(sql_db)
 
-app.include_router(get_register_router(get_auth_manager), prefix="/auth", tags=["Authentication"])
+class UserCreate(schema.BaseUserCreate):
+    pass 
+
+app.include_router(get_register_router(get_auth_manager, ), prefix="/auth", tags=["Authentication"])
